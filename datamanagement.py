29610cc089e71dfe45b0915c1f72af97c74f7854
Yuto-kimura <s1250172a@gmail.com>
2019-07-11 14:52:51 +0900

    Add one more new feature

--- datamanagement.py
+++ datamanagement.py
@@ -4,30 +4,38 @@ class MenuItem:
     def road(self):
         print("Rolling the dice...")
     def dais(self,daisA,daisB):
-        print("Die: "+str(daisA))
-        print("Die: "+str(daisB))
+        print("Die:"+str(daisA))
+        print("Die:"+str(daisB))
     def hello(self):
         print("Hello,"+self.name+"!")
-    def win(self):
-        print(self.name+" won!")
-    def lost(self):
-        print(self.name+" lost.")
+    def win(self,times,resu,win,lost):
+        print("After "+str(times)+" experiments, "+self.name+" won "+str(win)+" times, lost "+str(lost)+" times. And the probability of winning was "+str(resu)+"%.")
+        # print("So,"+self.name+" won!")
+    # def lost(self,times,avg,win,lost):
+    #     print("After "+str(times)+" experiments, "+self.name+" won"+str(win)+" times, lost"+str(lost)+" times and the average was "+str(avg)+" times.")
+        # print("So,"+self.name+" won!")
 
-
+win=0
+lost=0
+i=0
 
 menu_item1 = MenuItem()
-daisA = random.randint(1,6)
-daisB = random.randint(1,6)
-
-result = daisA + daisB
 print("What is your name?")
-menu_item1.name = input("> ")
+menu_item1.name = input(">")
 menu_item1.hello()
+times = int(input("How many times do you experiment? "))
 menu_item1.road()
-menu_item1.dais(daisA,daisB)
-print("Total value: "+str(result))
 
-if result > 7:
-    menu_item1.win()
-else:
-    menu_item1.lost()
+while i < times :
+    daisA = random.randint(1,6)
+    daisB = random.randint(1,6)
+    result = daisA + daisB
+    if result > 7:
+        win+=1
+    else:
+        lost+=1
+    i+=1
+
+resu = win + lost
+resu = float(win / resu)
+menu_item1.win(times,resu*100,win,lost)
